c9b731cbc9ccd653501eb33524a16b4fbab34d8a
samuel <samiehave9@gmail.com>
2024-07-11 17:08:53 +0300

    IF..IF..ELSFI..IF..ELSIF...ELSE

--- control-flow.rb
+++ control-flow.rb
@@ -14,3 +14,29 @@ if statement_to_be_evaluated == true then
 end
 # One line if statement
 if statement_to_be_evaluated == true then # do something end
+
+if 1 < 2 
+    puts "Yes, it is!"
+else
+    puts "No, it's not!"
+end    
+
+puts "Having fun with Ruby!" if 1 < 2
+
+puts "Hot diggity damn, 1 is less than 2" if 1 < 2
+
+# if..elsif Statement
+if attack_by_land == 'true'
+    puts "release the goat"
+else 
+    puts "release the shark"
+end
+
+# if...elsif...else
+if attack_by_land == 'true'
+    puts 'release the goat'
+elsif attack_by_water == 'true'
+    puts 'release the shark'
+else 
+    puts 'release kelvin the octopus'
+end
