0e154925defad2467f8f482fb284867a754abf41
samuel <samiehave9@gmail.com>
2024-07-11 13:43:30 +0300

    Create the control-flow.rb file and some conditional statements

---
+++ control-flow.rb
@@ -0,0 +1,16 @@
+# Control flow in Ruby
+# if, elsif, else
+# Bigger if statement
+if statement_to_be_evaluated == true
+    # do something
+elsif another
+    # do something else
+    else
+    # do something else
+end
+# Block if statement
+if statement_to_be_evaluated == true then
+    # do something
+end
+# One line if statement
+if statement_to_be_evaluated == true then # do something end
