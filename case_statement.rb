877420533a4f6476bcbd4113aae7069136f3f2d9
samuel <samiehave9@gmail.com>
2024-07-13 14:30:20 +0300

    CASE_STATEMENT

--- case_statement.rb
+++ case_statement.rb
@@ -1,7 +1,7 @@
 # CASE STATEMENTS
 a = 5
 
-case 5
+case a
 when a = 5
     puts 'a is 5'
 when a = 6
@@ -18,4 +18,25 @@ elsif a == 6
     puts 'a is 6'
 else 
     puts 'a is neither 5 nor 6'
+end
+# Assigning a variable
+ans = case  a
+when a = 5
+    'a is 5'
+when a = 6
+    'a is 6'
+else
+    'a is neither 5 nor 6'
+end
+puts ans
+# Here no need for the arguement on case
+#The difference here is that we don't provide an argument on line 5, and we have to fully test each value with a ==; most developers prefer to use if/elsif/else/end instead, but there are situations where this form is preferred. We won't get into that here.
+w = 9
+case
+when w == 9
+    puts 'w is 9'
+when w == 10
+    puts 'w is 10'
+else
+    puts 'w is nether 9 nor 10'
 end
