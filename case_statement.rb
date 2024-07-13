7415de322cae9c5281f09d0bc2fdac354adb064f
samuel <samiehave9@gmail.com>
2024-07-13 14:17:36 +0300

    Create case_statement.r file and demonstrate the case and if statements

---
+++ case_statement.rb
@@ -0,0 +1,21 @@
+# CASE STATEMENTS
+a = 5
+
+case 5
+when a = 5
+    puts 'a is 5'
+when a = 6
+    puts 'a = 6'
+else 
+    puts 'a is neither 5 nor 6'
+end
+# IF ELSIF ELSE END
+a = 5
+
+if a == 5
+    puts 'a is 5'
+elsif a == 6
+    puts 'a is 6'
+else 
+    puts 'a is neither 5 nor 6'
+end
